e95d0b75772fee1e2a64f814fcb300b8f3b4d3be
Michael Chang <michael@micbase.com>
2017-04-21 09:13:59 -0500

    Add HVAC manual/off mode

--- hvacAutomation.groovy
+++ hvacAutomation.groovy
@@ -8,58 +8,74 @@ definition(
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
 )
 
-preferences() {
-    section("Choose thermostat... ") {
-        input "thermostat", "capability.thermostat", required: true
-    }
-    section("Choose temperature sensor to use instead of the thermostat's... ") {
-        input "sensor", "capability.temperatureMeasurement", title: "Temperature Sensors", required: true
-    }
-    section("Choose outside temperature sensor... ") {
-        input "outside", "capability.temperatureMeasurement", title: "Outside Temperature Sensors", required: true
-    }
-    section("Heating threshold..." ) {
-        input "heatingThreshold", "decimal", title: "Degrees", required: true
-    }
-    section("Cooling threshold..." ) {
-        input "coolingThreshold", "decimal", title: "Degrees", required: true
-    }
+preferences {
+    page(name: "configurations", install: true, uninstall: true)
+}
 
-    section("Heat setting..." ) {
-        input "heatingSetpoint", "decimal", title: "Degrees", required: true
-    }
-    section("Heat setting at sleep..." ) {
-        input "sleepHeatingSetpoint", "decimal", title: "Degrees", required: true
-    }
-    section("Heat setting at away..." ) {
-        input "awayHeatingSetpoint", "decimal", title: "Degrees", required: true
-    }
-    section("Heat setting before sleep..." ) {
-        input "preSleepHeatingSetpoint", "decimal", title: "Degrees", required: true
-    }
-    section("Heat setting before morning..." ) {
-        input "preMorningHeatingSetpoint", "decimal", title: "Degrees", required: true
-    }
+def configurations() {
+    dynamicPage(name: "configurations", title: "Configurations") {
+        section("Choose thermostat... ") {
+            input "thermostat", "capability.thermostat", required: true
+        }
+        section("Choose temperature sensor to use instead of the thermostat's... ") {
+            input "sensor", "capability.temperatureMeasurement", title: "Temperature Sensors", required: true
+        }
+        section("Choose outside temperature sensor... ") {
+            input "outside", "capability.temperatureMeasurement", title: "Outside Temperature Sensors", required: true
+        }
+        section("Heating threshold..." ) {
+            input "heatingThreshold", "decimal", title: "Degrees", required: true
+        }
+        section("Cooling threshold..." ) {
+            input "coolingThreshold", "decimal", title: "Degrees", required: true
+        }
 
-    section("Air conditioning setting...") {
-        input "coolingSetpoint", "decimal", title: "Degrees", required: true
-    }
-    section("Air conditioning setting at sleep...") {
-        input "sleepCoolingSetpoint", "decimal", title: "Degrees", required: true
-    }
-    section("Air conditioning setting at away...") {
-        input "awayCoolingSetpoint", "decimal", title: "Degrees", required: true
-    }
-    section("Air conditioning setting before sleep...") {
-        input "preSleepCoolingSetpoint", "decimal", title: "Degrees", required: true
-    }
-    section("Air conditioning setting before morning...") {
-        input "preMorningCoolingSetpoint", "decimal", title: "Degrees", required: true
+        section("Heat setting..." ) {
+            input "heatingSetpoint", "decimal", title: "Degrees", required: true
+        }
+        section("Heat setting at sleep..." ) {
+            input "sleepHeatingSetpoint", "decimal", title: "Degrees", required: true
+        }
+        section("Heat setting at away..." ) {
+            input "awayHeatingSetpoint", "decimal", title: "Degrees", required: true
+        }
+        section("Heat setting before sleep..." ) {
+            input "preSleepHeatingSetpoint", "decimal", title: "Degrees", required: true
+        }
+        section("Heat setting before morning..." ) {
+            input "preMorningHeatingSetpoint", "decimal", title: "Degrees", required: true
+        }
+
+        section("Air conditioning setting...") {
+            input "coolingSetpoint", "decimal", title: "Degrees", required: true
+        }
+        section("Air conditioning setting at sleep...") {
+            input "sleepCoolingSetpoint", "decimal", title: "Degrees", required: true
+        }
+        section("Air conditioning setting at away...") {
+            input "awayCoolingSetpoint", "decimal", title: "Degrees", required: true
+        }
+        section("Air conditioning setting before sleep...") {
+            input "preSleepCoolingSetpoint", "decimal", title: "Degrees", required: true
+        }
+        section("Air conditioning setting before morning...") {
+            input "preMorningCoolingSetpoint", "decimal", title: "Degrees", required: true
+        }
+        def actions = location.helloHome?.getPhrases()*.label
+        if (actions) {
+            actions.sort()
+            section("Routine Bindings") {
+                input "automaticRoutine", "enum", title: "Routine to change mode to automatic", options: actions, required: false
+                input "offRoutine", "enum", title: "Routine to change mode to off", options: actions, required: false
+                input "manualRoutine", "enum", title: "Routine to change mode to manual", options: actions, required: false
+            }
+        }
     }
 }
 
 def installed() {
     subscribeEvents()
+    state.mode = "automatic"
 }
 
 def updated() {
@@ -70,6 +86,7 @@ def updated() {
 
 def subscribeEvents() {
     subscribe(location, "mode", changedLocationMode)
+    subscribe(location, "routineExecuted", routineChanged)
     subscribe(sensor, "temperature", temperatureHandler)
     subscribe(outside, "temperature", temperatureHandler)
     subscribe(thermostat, "temperature", temperatureHandler)
@@ -83,6 +100,23 @@ def changedLocationMode(evt) {
 
 def temperatureHandler(evt) {
     evaluate()
+}
+
+def routineChanged(evt) {
+    def lastState = state.mode
+    if (evt.displayName == automaticRoutine) {
+        state.mode = "automatic"
+    }
+    else if (evt.displayName == offRoutine) {
+        state.mode = "off"
+    }
+    else if (evt.displayName == manualRoutine) {
+        state.mode = "manual"
+    }
+    if (lastState != state.mode) {
+        log.debug("mode changed to: ${state.mode}")
+        evaluate()
+    }
 }
 
 private getHeatSetpoint() {
@@ -123,52 +157,62 @@ private evaluate() {
         "remote sensor temp: $sensor.currentTemperature, desire heat setpoint: $heatSetpoint, desire cool setpoint: $coolSetpoint, " +
         "outside temperature: $outside.currentTemperature")
 
-    if (outside.currentTemperature <= heatingThreshold) {
-        // change to heating mode
-        if (thermostat.currentThermostatMode != "heat") {
-            thermostat.heat()
-            log.debug("set to heat mode")
+    if (state.mode == "automatic") {
+        if (outside.currentTemperature <= heatingThreshold) {
+            // change to heating mode
+            if (thermostat.currentThermostatMode != "heat") {
+                thermostat.heat()
+                log.debug("set to heat mode")
+            }
+        } else if (outside.currentTemperature >= coolingThreshold) {
+            // change to cooling mode
+            if (thermostat.currentThermostatMode != "cool") {
+                thermostat.cool()
+                log.debug("set to cool mode")
+            }
+        } else {
+            // good weather, turn off
+            if (thermostat.currentThermostatMode != "off") {
+                thermostat.off()
+                log.debug("turn off")
+            }
         }
-    } else if (outside.currentTemperature >= coolingThreshold) {
-        // change to cooling mode
-        if (thermostat.currentThermostatMode != "cool") {
-            thermostat.cool()
-            log.debug("set to cool mode")
+
+        if (thermostat.currentThermostatMode in ["cool","auto"]) {
+            if (sensor.currentTemperature - coolSetpoint >= threshold) {
+                if (thermostat.currentCoolingSetpoint != thermostat.currentTemperature - 2) {
+                    thermostat.setCoolingSetpoint(thermostat.currentTemperature - 2)
+                    log.debug("cooling to ${thermostat.currentTemperature - 2}")
+                }
+            }
+            else if (coolSetpoint - sensor.currentTemperature >= 0) {
+                if (thermostat.currentCoolingSetpoint != thermostat.currentTemperature + 2) {
+                    thermostat.setCoolingSetpoint(thermostat.currentTemperature + 2)
+                    log.debug("idle to ${thermostat.currentTemperature + 2}")
+                }
+            }
         }
-    } else {
-        // good weather, turn off
+        if (thermostat.currentThermostatMode in ["heat","emergency heat","auto"]) {
+            if (heatSetpoint - sensor.currentTemperature >= threshold) {
+                if (thermostat.currentHeatingSetpoint != thermostat.currentTemperature + 2) {
+                    thermostat.setHeatingSetpoint(thermostat.currentTemperature + 2)
+                    log.debug("heating to ${thermostat.currentTemperature + 2}")
+                }
+            }
+            else if (sensor.currentTemperature - heatSetpoint >= 0) {
+                if (thermostat.currentHeatingSetpoint != thermostat.currentTemperature - 2) {
+                    thermostat.setHeatingSetpoint(thermostat.currentTemperature - 2)
+                    log.debug("idle to ${thermostat.currentTemperature - 2}")
+                }
+            }
+        }
+    } else if (state.mode == "off") {
+        log.debug("off mode, shut it down")
         if (thermostat.currentThermostatMode != "off") {
             thermostat.off()
             log.debug("turn off")
         }
-    }
-
-    if (thermostat.currentThermostatMode in ["cool","auto"]) {
-        if (sensor.currentTemperature - coolSetpoint >= threshold) {
-            if (thermostat.currentCoolingSetpoint != thermostat.currentTemperature - 2) {
-                thermostat.setCoolingSetpoint(thermostat.currentTemperature - 2)
-                log.debug("cooling to ${thermostat.currentTemperature - 2}")
-            }
-        }
-        else if (coolSetpoint - sensor.currentTemperature >= 0) {
-            if (thermostat.currentCoolingSetpoint != thermostat.currentTemperature + 2) {
-                thermostat.setCoolingSetpoint(thermostat.currentTemperature + 2)
-                log.debug("idle to ${thermostat.currentTemperature + 2}")
-            }
-        }
-    }
-    if (thermostat.currentThermostatMode in ["heat","emergency heat","auto"]) {
-        if (heatSetpoint - sensor.currentTemperature >= threshold) {
-            if (thermostat.currentHeatingSetpoint != thermostat.currentTemperature + 2) {
-                thermostat.setHeatingSetpoint(thermostat.currentTemperature + 2)
-                log.debug("heating to ${thermostat.currentTemperature + 2}")
-            }
-        }
-        else if (sensor.currentTemperature - heatSetpoint >= 0) {
-            if (thermostat.currentHeatingSetpoint != thermostat.currentTemperature - 2) {
-                thermostat.setHeatingSetpoint(thermostat.currentTemperature - 2)
-                log.debug("idle to ${thermostat.currentTemperature - 2}")
-            }
-        }
+    } else if (state.mode == "manual") {
+        log.debug("manual mode, nothing to do")
     }
 }
